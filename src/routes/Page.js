e3e4685367dd8d77062d6863f9439a1eace18a23
Ryan Hansen <rphansen91@gmail.com>
2018-09-13 15:25:58 -0700

    optional footer

--- src/routes/Page.js
+++ src/routes/Page.js
@@ -4,10 +4,10 @@ import Footer from "../components/Footer";
 import Links from "./Links";
 import s from "./route.css";
 
-export default ({ children, links, footer = true }) => (
+export default ({ children, links, footer = <Footer /> }) => (
   <div className={s.page}>
     <Navbar>{links || Links}</Navbar>
     <div className={s.main}>{children}</div>
-    {footer && <Footer />}
+    {footer}
   </div>
 );
